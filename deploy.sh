3db027e7590e2959e4706cab16481e7e6482b993
ashton <ashton.arlo@supportsages.com>
2025-03-25 02:23:41 +0530

    add test template.yaml

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 echo "Processing folders..."
+CHANGED_FOLDERS=$(echo "$CHANGED_FOLDERS" | grep -v '^RESOURCE-SETUP$')
 for folder in $CHANGED_FOLDERS; do
-  CHANGED_FOLDERS=$(echo "$CHANGED_FOLDERS" | grep -v '^RESOURCE-SETUP$')
   echo "Processing folder: $folder"
   if [ -f "$folder/template.yaml" ]; then
     echo "Found template.yaml in $folder. Running SAM build and deploy..."
     TemplatePath="$folder/template.yaml"
     sam build -t $TemplatePath
-    SAM_CLI_POLL_DELAY=5 sam deploy --stack-name "ss-devops-backend-sam-stack" --s3-bucket $Bucket --s3-prefix ss-devops-backend --capabilities CAPABILITY_IAM
+    SAM_CLI_POLL_DELAY=5 sam deploy --stack-name "$folder-sam-stack" --s3-bucket $Bucket --s3-prefix $folder-sam-stack --capabilities CAPABILITY_IAM
   else
     echo "No template.yaml found in $folder. Skipping."
   fi
