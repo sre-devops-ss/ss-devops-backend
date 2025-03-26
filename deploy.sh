1d7845fabc77876f05ccd565d3e5556879614c0a
ashton <ashton.arlo@supportsages.com>
2025-03-27 01:05:12 +0530

    role-creation-common

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,18 @@ for folder in $CHANGED_FOLDERS; do
     echo "Found template.yaml in $folder. Running SAM build and deploy..."
     TemplatePath="$folder/template.yaml"
     sam build -t $TemplatePath
-    SAM_CLI_POLL_DELAY=5 sam deploy --stack-name "$folder-sam-stack" --s3-bucket $Bucket --s3-prefix $folder-sam-stack --capabilities CAPABILITY_IAM
+    SAM_CLI_POLL_DELAY=5 sam deploy --stack-name "$folder-sam-stack" --s3-bucket $Bucket --s3-prefix $folder-sam-stack --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM
   else
     echo "No template.yaml found in $folder. Skipping."
   fi
+
+    if [ -f "$folder/domain.yaml" ]; then
+      echo "Found domain.yaml in $folder. Running SAM build and deploy..."
+      DomainTemplatePath="$folder/domain.yaml"
+      sam build -t $DomainTemplatePath
+      SAM_CLI_POLL_DELAY=5 sam deploy --stack-name "$folder-domain-sam-stack" --s3-bucket $Bucket --s3-prefix $folder-domain-sam-stack --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM
+    else
+      echo "No domain.yaml found in $folder. Skipping."
+    fi
+
 done
