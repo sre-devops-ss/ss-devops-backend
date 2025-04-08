f8953ce108324114a5b754a813dbbed63b00661e
ashton <ashton.arlo@supportsages.com>
2025-04-08 14:00:19 +0530

    update auth api- domainmapping - user

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ for folder in $CHANGED_FOLDERS; do
     echo "Found template.yaml in $folder. Running SAM build and deploy..."
     TemplatePath="$folder/template.yaml"
     sam build -t $TemplatePath
-    SAM_CLI_POLL_DELAY=5 sam deploy --stack-name "$folder-sam-stack" --s3-bucket $Bucket --s3-prefix $folder-sam-stack --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM
+    SAM_CLI_POLL_DELAY=5 sam deploy --stack-name "$folder-sam-stack" --s3-bucket $Bucket --s3-prefix $folder-sam-stack --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM --parameter-overrides Environment=$Environment
   else
     echo "No template.yaml found in $folder. Skipping."
   fi
@@ -16,7 +16,7 @@ for folder in $CHANGED_FOLDERS; do
       echo "Found domain.yaml in $folder. Running SAM build and deploy..."
       DomainTemplatePath="$folder/domain.yaml"
       sam build -t $DomainTemplatePath
-      SAM_CLI_POLL_DELAY=5 sam deploy --stack-name "$folder-domain-sam-stack" --s3-bucket $Bucket --s3-prefix $folder-domain-sam-stack --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM
+      SAM_CLI_POLL_DELAY=5 sam deploy --stack-name "$folder-domain-sam-stack" --s3-bucket $Bucket --s3-prefix $folder-domain-sam-stack --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM --parameter-overrides Environment=$Environment
     else
       echo "No domain.yaml found in $folder. Skipping."
     fi
