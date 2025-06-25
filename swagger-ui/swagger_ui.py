b2d4689f25f956621976578a66ced73d11cb4818
ashton <ashton.arlo@supportsages.com>
2025-06-25 17:17:42 +0530

    update swagger

--- swagger-ui/swagger_ui.py
+++ swagger-ui/swagger_ui.py
@@ -1,19 +1,11 @@
-import json
-import yaml
-
 def lambda_handler(event, context):
     with open("index.html", "r") as f:
         html = f.read()
-
-    with open("openapi.yaml", "r") as f:
-        openapi_data = yaml.safe_load(f)
-
-    rendered_html = html.replace("__SWAGGER_JSON__", json.dumps(openapi_data))
 
     return {
         "statusCode": 200,
         "headers": {
             "Content-Type": "text/html"
         },
-        "body": rendered_html
+        "body": html
     }
