4d7c7f7420297853e010ccb2886c8fa5a9046c85
ashton <ashton.arlo@supportsages.com>
2025-09-18 12:29:22 +0530

    update

--- aws/fileprocess-api/process.py
+++ aws/fileprocess-api/process.py
@@ -41,7 +41,6 @@ def lambda_handler(event, context):
         IamInstanceProfile={'Arn': instance_role},
         UserData=base64.b64encode(userdata.encode("utf-8")).decode("utf-8"),
         SubnetId=subnet_id,
-        VpcId=vpc_id,
         SecurityGroupIds= [sg_id]
 
 
